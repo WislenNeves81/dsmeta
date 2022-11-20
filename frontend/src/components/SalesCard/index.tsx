5cf52037ace12066e83ee98914a46f10299fbb4c
Wislen Neves <wislen.neves@gmail.com>
2022-11-20 18:04:44 -0300

    data fix

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -12,7 +12,7 @@ function SalesCard() {
 
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
     const max = new Date();
-    const [minDate, setMinDate] = useState(new Date());
+    const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
     const [sales, setSales] = useState<Sale[]>([]);
